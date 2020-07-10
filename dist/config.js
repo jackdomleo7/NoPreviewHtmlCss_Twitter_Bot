293aadc2d7de3a6059e5262d16097c9089e5f198
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2020-07-10 13:16:18 +0100

    Ignore dist fromt git

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-  consumer_key: 'mBXqG1Hte7TbD4btYEn7rZkkg',
-  consumer_secret: 'MP0h9FrCFzpAcRpNwnF5aAu6kOr5MXgJoh7gGBV4qVizoAnzD2',
-  access_token: '1280850874026397696-LbfLsFWWlO8hJuT55hHqhWhLZRLS5k',
-  access_token_secret: 'mkLrthSh7RWVwAgnvP9Q1vzxAkkNAcM62hZNmaGyUoaYu'
-};
